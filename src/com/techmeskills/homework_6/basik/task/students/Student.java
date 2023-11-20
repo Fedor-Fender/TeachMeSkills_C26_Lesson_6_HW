9739027c34347472bea33b2f776e3a7251421848
Fedor-Fender <developerc26java@gmail.com>
2023-11-20 10:53:09 +0300

    Second commit
    task_2 creating program are students of group

---
+++ src/com/techmeskills/homework_6/basik/task/students/Student.java
@@ -0,0 +1,24 @@
+package com.techmeskills.homework_6.basik.task.students;
+
+import java.util.Arrays;
+
+public class Student {
+    String firstname;
+    String surname;
+    String idPasport;
+    String nameGroup;
+
+    // to create constructor
+    public Student(String name,String surname,String idPasport,String nameGroup) {
+        this.firstname = firstname;
+        this.surname = surname;
+        this.idPasport = idPasport;
+        this.nameGroup = nameGroup;
+    }
+
+    public void outputStudent(String[][]matrix) {
+        for (String[] name:matrix) {
+        } System.out.println(Arrays.deepToString(matrix));
+    }
+}
+
